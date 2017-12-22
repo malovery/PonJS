6b83f285436898a21109a9faee7a94a51ab96abd
Sergey <zergis2014@gmail.com>
2017-12-22 17:41:13 +0300

    Готово

---
+++ js/PonJS.js
@@ -0,0 +1,56 @@
+/*var y=1,
+x=y=tepeof x;
+console.log(x);*/
+/*	var rocet='war',
+	var note=30;
+	colsole.log(var=rocet+note)*/
+
+	/*var basket='6',
+		var note=3,
+			var bas=2;
+			result=basket+note*bas
+			console.log(result);*/
+
+/*			var count=3;
+				count *=7;
+				count +=2;
+				count++;
+				console.log(count);  */
+
+/*			var count=100;
+				count -=97;
+				count +=2;
+				count=4;
+				console.log('itog='+count);              */
+
+/*				<script type="text/javascript">
+				var a=1;
+				var b=a++ +1;
+				alert(b);
+				</script>*/
+
+
+/*				<script type="text/javascript">
+				var a=1;
+				var b=++a +1;
+				alert(b);
+				</script>*/
+	/* Задание 1
+	var day =365, zem1="Земля",
+			kol="7 млрд.",plan="Солнца";
+			str1=" Мы живем на планете ";
+			str2=" она делеет один оборот вокруг ";
+			str3=" за ";
+			str4=" дней ";
+			str5=" Население нашей планеты составляет примерно ";
+			str6=" человек ";
+			str7=".";
+			str8=",";
+		//	console.log(str1+zem1+str8+str2+plan+str3+day+str4+str7+str5+kol+str6+str7);*/
+console.log(str1,zem1,str8,str2,plan,str3,day,str4,str7,str5,kol,str6,str7 );*//* Задание 2*/
+
+Задание   2
+
+var admin, name="Василий";
+admin = name;
+colsole.log(admin);
